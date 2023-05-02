42d424475df26c551a53f316f032de0b83c06010
+ <+>
2023-05-02 00:18:29 -0400

    completed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,15 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie,index)=>{
+    return <div key={index}>
+      {movie.title}
+      {movie.time}
+      <ul ><li>{movie.genres}</li></ul>
+    </div>
+  })}
+  </div>;
 }
-
 export default Movies;
